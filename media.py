899b1dabaa181506a6824303309ff109c06ee2c5
qiaop <qiaopeng0809@gmail.com>
2017-10-16 17:17:24 +0800

    添加注释

--- media.py
+++ media.py
@@ -6,10 +6,23 @@ class Movie():
     """This class provides a way to store movie related information"""
 
     def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
+        """Constructor
+
+        Keyword arguments:
+        self -- instance self
+        movie_title -- 电影标题
+        movie_storyline -- 故事简介
+        poster_image -- 海报图片链接
+        trailer_youtube -- 预告片YouTube播放链接
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """show trailer，Open the webbrowser to play the vedio
+        Keyword arguments:
+        self -- instance self
+        """
         webbrowser.open(self.trailer_youtube_url)
